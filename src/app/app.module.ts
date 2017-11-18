51a68c5fe23f200a017cf496c2d2f6f15b357320
Sanidhya Mangal <sandyrockstar19@gmail.com>
2017-11-18 15:18:38 +0530

    updated sw

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { AngularFireAuthModule } from 'angularfire2/auth';
-
+import { ServiceWorkerModule } from '@angular/service-worker';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FormService } from './form.service';
@@ -27,7 +27,8 @@ import { environment } from '../environments/environment';
     HttpClientModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireAuthModule,
-    AngularFireDatabaseModule
+    AngularFireDatabaseModule,
+    ServiceWorkerModule
   ],
   providers: [
     FormService
